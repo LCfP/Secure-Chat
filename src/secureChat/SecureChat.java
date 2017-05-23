24475f92fbd26795d629af38af2518c1f2532e61
s3183807 <s3183807@WKS022362.workspace.rug.nl>
2017-05-23 20:42:24 +0200

    Added conversation pane with columns for sender, message and time

--- src/secureChat/SecureChat.java
+++ src/secureChat/SecureChat.java
@@ -36,6 +36,10 @@ public class SecureChat extends Application{
 	public static Button messageButton;
 	public static ScrollPane messageHistory;
 	public static ScrollPane users;
+	public static GridPane conversationPane;
+	public static Label senderInput;
+	public static Label messageInput;
+	public static Label timeInput;
 
 	public static void main(String[] args)
 	{
@@ -71,6 +75,26 @@ public class SecureChat extends Application{
 		messagePane.setAlignment(Pos.CENTER);
 		messagePane.getChildren().addAll(messageField,messageButton);
 		root.setBottom(messagePane);
+		
+		conversationPane = new GridPane();
+		conversationPane.setPrefWidth(600);
+		conversationPane.setPrefHeight(450);
+		messageHistory.setContent(conversationPane);
+		
+		// TODO replace placeholders with actual sender, message, and time
+		senderInput = new Label("Peterkfdjsa;lkdfjsa;kdfjsa;kdfjsa;kdfjs;kjdfs");
+		messageInput = new Label("Hi");
+		timeInput = new Label("20.15");
+		
+		GridPane.setConstraints(senderInput, 0, 0);
+		GridPane.setConstraints(messageInput, 1, 0);
+		GridPane.setConstraints(timeInput, 2, 0);
+		
+		conversationPane.getColumnConstraints().add(new ColumnConstraints(100));
+		conversationPane.getColumnConstraints().add(new ColumnConstraints(450));
+		conversationPane.getColumnConstraints().add(new ColumnConstraints(50));
+		
+		conversationPane.getChildren().addAll(senderInput, messageInput, timeInput);
 		
 		Stage loginStage = new Stage();
 		GridPane loginPane = new GridPane();
